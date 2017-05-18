f1f3dc1dfd71a7ebf6fb3822fea61762dba804ae
yuzu.ka <yuzuzu.ka.work@gmail.com>
2017-05-18 06:51:31 +0000

    post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170512093134) do
+ActiveRecord::Schema.define(version: 20170518061150) do
+
+  create_table "microposts", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string "content"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_microposts_on_user_id"
+  end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
@@ -20,4 +28,5 @@ ActiveRecord::Schema.define(version: 20170512093134) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "microposts", "users"
 end
