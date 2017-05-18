f21752805def7cd2ba33c03f33e7b770ddc17401
yuzu.ka <yuzuzu.ka.work@gmail.com>
2017-05-18 06:09:44 +0000

    user login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,32 @@ class SessionsController < ApplicationController
   end
 
   def create
+    email = params[:session][:email].downcase
+    password = params[:session][:password]
+    if login(email,password)
+      flash[:success] = 'ログインに成功しました'
+      redirect_to @user
+    else
+      flash.now[:danger] = 'ログインに失敗しました'
+      render 'new'
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    flash[:success] = 'ログアウトしました'
+    redirect_to root_path
+  end
+end
+
+private
+
+def login(email,password)
+  @user = User.find_by(email: email)
+  if @user && @user.authenticate(password)
+    session[:user_id] = @user.id
+    return true
+  else
+    return false
   end
 end
